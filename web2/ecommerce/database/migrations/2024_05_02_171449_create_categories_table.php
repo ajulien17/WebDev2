23b6fee670ad2f62e67836225dc8a2f0fc96fadf
ajulien17 <anthonyjulien777@gmail.com>
2024-05-03 15:08:39 +0300

    Second commit

---
+++ web2/ecommerce/database/migrations/2024_05_02_171449_create_categories_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+
+            $table->integer('parent_id');
+            $table->integer('section_id');
+            $table->string('category_name');
+            $table->string('category_image');
+            $table->double('category_discount')->default(0);
+            $table->text('description')->nullable();
+            $table->string('url'); 
+            $table->string('meta_title')->nullable(); 
+            $table->string('meta_description')->nullable(); 
+            $table->string('meta_keywords')->nullable();
+            $table->tinyInteger('status')->default(1);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+};
